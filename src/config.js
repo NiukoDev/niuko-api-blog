7aae9da702b754971fe5cc3ba9be97e48c6de624
alfonsoHR98 <alfonsohr_98@outlook.com>
2024-11-20 16:33:02 -0600

    Add separate origin constants for blog and dashboard in config

--- src/config.js
+++ src/config.js
@@ -3,7 +3,8 @@ config();
 
 export const DATABASE_URL = process.env.DATABASE_URL;
 export const PORT = process.env.PORT;
-export const ORIGIN = process.env.ORIGIN;
+export const ORIGIN_BLOG = process.env.ORIGIN_BLOG;
+export const ORIGIN_DASHBOARD = process.env.ORIGIN_DASHBOARD;
 export const MAILGUN_API_KEY = process.env.MAILGUN_API_KEY;
 export const MAILGUN_DOMAIN = process.env.MAILGUN_DOMAIN;
 export const MAILGUN_RECIPIENT = process.env.MAILGUN_RECIPIENT;
