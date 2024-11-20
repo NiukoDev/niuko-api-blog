cb3a6eb1d11151e0ce6c3a2797ee27a4fc783915
alfonsoHR98 <alfonsohr_98@outlook.com>
2024-11-20 17:19:17 -0600

    Remove build script from package.json and update console log message in index.js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import app from "./app.js";
 import { PORT } from "./config.js";
 
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en el puerto ${PORT}`);
+  console.log(`Servidor corriendo en el puerto: ${PORT}`);
 });
+// "build": "prisma generate && npm run start",
